efa57b5c1701fc52c5fd861e08ac1ac21d057511
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-07-02 10:15:21 +0300

    Update factors.py

--- factors.py
+++ factors.py
@@ -4,6 +4,8 @@ import sys
 file = open(sys.argv[1], "r")
 readed = file.read().split("\n")
 for line in readed:
+    if line == "":
+        break
     line = int(line)
     for q in range(2, line):
         if line % q == 0:
