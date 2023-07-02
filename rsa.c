eb2f35dc296e23d5aea8c64a1fdee4a73dd4cb0b
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-07-02 09:14:56 +0300

    test

--- rsa.c
+++ rsa.c
@@ -1,45 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <gmp.h>
 
-int is_prime(unsigned long number);
+int is_prime(mpz_t num)
+{
+	mpz_t i, sqrt_num;
+	mpz_inits(i, sqrt_num, NULL);
 
-/**
- * main - entry point to the program
- * @argc: argument count
- * @argv: arguments passed to the program
- */
-int main(int argc, char **argv)
+	mpz_sqrt(sqrt_num, num);
+	for (mpz_set_ui(i, 2); mpz_cmp(i, sqrt_num) <= 0; mpz_add_ui(i, i, 1))
+	{
+		if (mpz_divisible_p(num, i))
+		{
+			mpz_clears(i, sqrt_num, NULL);
+			return 0;
+		}
+	}
+
+	mpz_clears(i, sqrt_num, NULL);
+	return 1;
+}
+
+int main(int argc, char *argv[])
 {
 	FILE *file = NULL;
 	char *buff_line = NULL;
 	size_t buff_size = 0;
-	unsigned long num = 0, p = 0, q = 0;
+	mpz_t number, q, p;
+	mpz_inits(number, q, p, NULL);
 
-	if (argc != 2)
+	if (argc < 2)
 	{
-		fprintf(stderr, "Usage: rsa_factors <file>\n");
-		return (1);
+		printf("Usage: %s <filename>\n", argv[0]);
+		return 1;
 	}
 
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
-		fprintf(stderr, "Error: Failed to open file\n");
-		return (1);
+		printf("Error opening file: %s\n", argv[1]);
+		return 1;
 	}
 
 	while (getline(&buff_line, &buff_size, file) != -1)
 	{
-		num = strtoul(buff_line, NULL, 10);
-
-		for (q = 2; q <= num / 2; q++)
+		if (buff_line[strlen(buff_line) - 1] == '\n')
 		{
-			if (is_prime(q) && num % q == 0)
+			buff_line[strlen(buff_line) - 1] = '\0';
+		}
+
+		mpz_set_str(number, buff_line, 10);
+
+		for (mpz_set_ui(q, 2); mpz_cmp(q, number) < 0; mpz_add_ui(q, q, 1))
+		{
+			if (mpz_divisible_p(number, q) && is_prime(q))
 			{
-				p = num / q;
+				mpz_divexact(p, number, q);
 				if (is_prime(p))
 				{
-					printf("%lu=%lu*%lu\n", num, p, q);
+					gmp_printf("%s=%Zd*%Zd\n", buff_line, p, q);
 					break;
 				}
 			}
@@ -48,30 +68,7 @@ int main(int argc, char **argv)
 
 	fclose(file);
 	free(buff_line);
+	mpz_clears(number, q, p, NULL);
+
 	return 0;
-}
-
-/**
- * is_prime - function to check if number is prime.
- * @number: number to check.
- * Return: 1 if prime, 0 if not.
- */
-int is_prime(unsigned long number)
-{
-	unsigned long i;
-
-	if (number < 2)
-	{
-		return (0);
-	}
-
-	for (i = 2; i * i <= number; i++)
-	{
-		if (number % i == 0)
-		{
-			return (0);
-		}
-	}
-
-	return (1);
 }
